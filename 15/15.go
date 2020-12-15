9c66c3b90abd8cd656b059756726b4196826b3f7
Stevenson Jean-Pierre <stevenson.jeanpierre@gmail.com>
2020-12-15 11:52:45 -0500

    Day 15

---
+++ 15/15.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/sjeanpierre/AOC2020/helpers"
+)
+
+var PART2 = true
+
+func main() {
+	var t []int
+	lines := helpers.LoadFileLines("./input.txt")
+	for _, v := range strings.Split(lines[0], ",") {
+		t = append(t, helpers.StringToInt(v))
+	}
+	process(t)
+}
+
+func process(lines []int) {
+	findPos := 2020
+	if PART2 {
+		findPos = 30000000
+	}
+
+	var tracker = make(map[int]int)
+	for i, v := range lines {
+		tracker[v] = i + 1
+	}
+	num := 0
+	for x := len(lines)+1; x > 0; x++ {
+		if x > findPos-3 && x < findPos+3 {
+			fmt.Println(x, ":", num)
+		}
+
+		if x > findPos+3 {
+			break
+		}
+		lastPos, ok := tracker[num]
+		if !ok {
+			tracker[num] = x
+			num = 0
+			continue
+		}
+		tracker[num]=x
+		num = x-lastPos
+	}
+}
